828d21dd4f38b94b712dbfe88ca8f9ee475de978
MrNasch <nicolaspaschal@hotmail.com>
2019-08-02 01:52:49 +0200

    saving image to storage

--- Travellers/Controller/ProfileViewController.swift
+++ Travellers/Controller/ProfileViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+//KINGFISHER
+
 
 class ProfileViewController: UIViewController {
     var user: User?
@@ -19,20 +21,22 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var lastname: UILabel!
     @IBOutlet weak var country: UILabel!
     @IBOutlet weak var profilePicture: UIImageView!
-    
-    
+    @IBOutlet weak var bioText: UITextView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bioText.delegate = self as? UITextViewDelegate
         db = Firestore.firestore()
-        let imageRef = storageRef.child("image")
+        profilePicture.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(selectProfileImage)))
+        
         // checking user info
         Auth.auth().addStateDidChangeListener { (auth, user) in
             guard let user = user else { return }
             self.user = User(authData: user)
         }
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         if FirebaseHelper().connected() != nil {
             // user is connected get user
@@ -45,8 +49,12 @@ class ProfileViewController: UIViewController {
                     let dataDescription = document.data()
                     let firstname = dataDescription!["Firstname"] as? String ?? ""
                     let lastname = dataDescription!["Lastname"] as? String ?? ""
+                    let bioText = dataDescription!["Bio"] as? String ?? ""
+                    //let profile = dataDescription!["Profil"] as? String ?? ""
                     self.firstname.text = firstname.capitalized
                     self.lastname.text = lastname.capitalized
+                    self.bioText.text = bioText
+                    //self.profile = setImage
                     print("Document data: \(String(describing: dataDescription))")
                 }
             }
@@ -56,15 +64,78 @@ class ProfileViewController: UIViewController {
         }
     }
     
+    // modify bio text and save it to firebase
+    func modifyBioText() {
+        bioText.delegate?.textViewDidEndEditing?(bioText)
+        let dataToSave: [String: Any] = ["Bio": bioText.text ?? "About me"]
+        guard let user = user else { return }
+        self.db.collection("users").document("\(user.uid)").setData(dataToSave, merge: true, completion: { (error) in
+            if let error = error {
+                print("noooooooo \(error.localizedDescription)")
+            }
+        })
+    }
+    
     // add profil picture
-//    func addProfilPicture() {
-//        let dataToSave: [String: Any] = ["Profil": "PHOTO"]
-//        self.db.collection("users").document("\(user.uid)").setData(dataToSave, merge: true, completion: { (error) in
-//            if let error = error {
-//                print("noooooooo \(error.localizedDescription)")
-//            }
-//        })
-//    }
+    @objc func selectProfileImage() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        
+        let actionSheet = UIAlertController(title: "Photo source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            } else {
+                let alert = UIAlertController(title: "Missing camera", message: "There is no camera available!", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                
+                self.present(alert, animated: true)
+            }
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Photo library", style: .default, handler: { (action:UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    func uploadImageProfile() {
+        // Reference to folder of profile image
+        guard let userId = user?.uid else { return }
+        let profileImageRef = storageRef.child("profileImage/\(userId).jpg")
+        
+        // File located on disk
+        guard let data = profilePicture.image?.jpegData(compressionQuality: 0.5) else {
+            return
+        }
+        
+        // Upload the file to the path "images/rivers.jpg"
+        let uploadTask = profileImageRef.putData(data, metadata: nil) { (metadata, error) in
+            //.putFile(from: localFile, metadata: nil) { metadata, error in
+            guard let metadata = metadata else { return }
+            // Metadata contains file metadata such as size, content-type.
+            let size = metadata.size
+            // You can also access to download URL after upload.
+            profileImageRef.downloadURL { (url, error) in
+                guard let downloadURL = url else { return }
+                
+                //        let dataToSave: [String: Any] = ["Profil": "downloadURL"]
+                //        guard let user = user else { return }
+                //        self.db.collection("users").document("\(user.uid)").setData(dataToSave, merge: true, completion: { (error) in
+                //            if let error = error {
+                //                print("noooooooo \(error.localizedDescription)")
+                //            }
+                //        })
+            }
+        }
+        
+        
+    }
+    
+    
+    
     // Disconnect user
     @IBAction func disconnectButton(_ sender: UIButton) {
         let user = Auth.auth()
@@ -74,5 +145,29 @@ class ProfileViewController: UIViewController {
         } catch let error {
             print("sign out failed", error.localizedDescription)
         }
+    }
+    
+    // dismiss keyboard when clic away have to make the view a TapgestureRecognizer
+    @IBAction func DismissKeyboard(_ sender: UITapGestureRecognizer) {
+        bioText.resignFirstResponder()
+        modifyBioText()
+    }
+}
+
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let imageChoose = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        
+        
+        profilePicture.image = imageChoose
+        
+        
+        uploadImageProfile()
+        
+        
+        picker.dismiss(animated: true, completion: nil)
     }
 }
